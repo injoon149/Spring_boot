e385282ccab566ad4bc3d93fde01f2a48d76bb50
dlswns <dlswns8679@naver.com>
2023-08-25 22:58:24 +0900

    feat - 페이징과 정렬

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,10 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 import study.datajpa.domain.Member;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.repository.MemberRepository;
 
 @RestController
@@ -16,4 +19,17 @@ public class MemberController {
     public String findMember(@PathVariable("id")Member member) {
         return member.getUsername();
     }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 12, sort = "username",
+    direction = Sort.Direction.DESC) Pageable pageable)
+    {
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> pageDto = page.map(member -> new MemberDto(member.getId(), member.getUsername(),
+                member.getTeam().getName()));
+        return pageDto;
+    }
+
+
+
 }
