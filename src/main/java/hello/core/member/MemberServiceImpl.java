60ca1a12829d7f6d32b8c267a64009415ab2029f
injoon <dlswns8679@naver.com>
2024-06-11 16:43:37 +0900

    스프링 - 의존관계 관련 학습

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,21 @@
 package hello.core.member;
 
-public class MemberServiceImpl implements MemberService{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 
-    public MemberServiceImpl(MemberRepository memberRepository){
+    @Autowired
+    public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
@@ -16,4 +25,6 @@ public class MemberServiceImpl implements MemberService{
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+
 }
