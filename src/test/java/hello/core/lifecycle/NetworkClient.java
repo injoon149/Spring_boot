be35844920f94b5fc19ce2176d857b17713bbbd9
INJOON CHOI <dlswns8679@naver.com>
2024-08-27 22:44:01 +0900

    spring

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,17 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+public class NetworkClient{
 
     private String url;
 
-    public NetworkClient(String url) {
+
+
+    public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
     public void setUrl(String url) {
@@ -26,5 +30,18 @@ public class NetworkClient {
     //서비스 종료 시 호출
     public void disconnect() {
         System.out.println("Disconnecting from " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
